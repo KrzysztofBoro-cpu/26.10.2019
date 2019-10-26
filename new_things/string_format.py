9324fdf5a213557df41f098054d7a0e254d9a8fd
Krzysiek <krzysztof.boron9@gmail.com>
2019-10-26 11:20:13 +0200

    Dodaliśmy plik new_things w katalogu

---
+++ new_things/string_format.py
@@ -0,0 +1,14 @@
+# x = 5
+# krotka = ("element", "drugi element")
+#
+# ciag_znakow = "Nasza liczba to {} blabla balbal {}, {}"
+#
+# print(f"Nasza liczba to {x + 10} blabla balbal {krotka}")
+# print("Nasza liczba to {} blabla {}, {}".format(x+15, krotka, {2: "sd"}))
+
+Name = input("Wprowasź imię:")
+Nazwisko = input("Wprowadź nazwisko:")
+
+print("Twoje imie", Name, "Twoje Nazwisko", Nazwisko)
+print(f"Twoje Imie {Name.capitalize()} Twoje Nazwisko {Nazwisko.capitalize()}")
+
